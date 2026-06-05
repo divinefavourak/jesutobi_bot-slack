31b329580bb6325ce944da8fa4fcfe072d4c1c65
divinefavourak <divinefavourakanbi07@gmail.com>
2026-06-05 23:00:12 +0100

    fix: correct command registration and API route imports

--- gateway/commands.js
+++ gateway/commands.js
@@ -1,27 +1,31 @@
 const axios = require("axios");
-const { route } = require("..router/eventRouter");
+const { route } = require("../router/eventRouter");
 
 module.exports = function registerCommands(app) {
-    app.command("dsb-ping", async ({ command, ack, respond}) => {
+
+    app.command("/dsb-ping", async ({ ack, respond }) => {
+        console.log("Received command: /dsb-ping");
         const start = Date.now();
         await ack();
         const latency = Date.now() - start;
-        await respond({ text: `Pong! Latency: ${latency}ms`});
-
+        await respond({ text: `Pong! Latency: ${latency}ms` });
     });
-    app.command("dsb-catfact", async ({ ack, respond}) => {
+
+    app.command("/dsb-catfact", async ({ ack, respond }) => {
         await ack();
-        const result = await route({ type: "catfact"});
+        const result = await route({ type: "catfact" });
         await respond({ text: result });
     });
+
     app.command("/joke", async ({ ack, respond }) => {
         await ack();
-        const result = await route({ type: "joke"});
-        await respond({ text: result})
+        const result = await route({ type: "joke" });
+        await respond({ text: result });
     });
-    app.command("help", async ({ ack, respond}) => {
+
+    app.command("/help", async ({ ack, respond }) => {
         await ack();
-        const result = await route ({ type: "help"});
-        await respond({ text:result});
+        const result = await route({ type: "help" });
+        await respond({ text: result });
     });
 };
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
 
 const { App } = require("@slack/bolt");
+//sanity check
+console.log("BOT TOKEN loaded:", !!process.env.SLACK_BOT_TOKEN);
+console.log("APP TOKEN loaded:", !!process.env.SLACK_APP_TOKEN);
+const registerCommands = require("./gateway/commands");
 
 const app = new App({
-    token: process.env.SLACK_BOLT_TOKEN,
+    token: process.env.SLACK_BOT_TOKEN,
     appToken: process.env.SLACK_APP_TOKEN,
     socketMode: true
 });
